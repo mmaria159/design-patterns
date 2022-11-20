a0c9f0afc9d88df975a6a9caeef6135118ea0600
Maria Macovei <macovei.maria1999@gmail.com>
2022-11-20 15:47:41 +0200

    first commit
    first commit

---
+++ src/main/java/com/mariamacovei/patterns/HeadFirstDesignPatternsSpringApplication.java
@@ -0,0 +1,11 @@
+package com.mariamacovei.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadFirstDesignPatternsSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HeadFirstDesignPatternsSpringApplication.class, args);
+    }
+}
